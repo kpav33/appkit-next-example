5c271dfd515d4724df471e2d969118cde230cfc1
Klemen Pavlovic <klemenpavlovic@gmail.com>
2024-09-12 13:49:34 +0200

    Deploy did.json file

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,15 @@ import Hooks from "@/components/Hooks";
 
 // In public folder in /.well-known/walletconnect.txt we have a code for domain verification purposes for wallet connect Verify API
 
+// https://docs.walletconnect.com/appkit/next/cloud/relay => For relay servers, unlikely we will need this? The Relay API specifically acts as a decentralized WebSocket-based messaging layer. It enables real-time communication between dApps and wallets by relaying encrypted messages through nodes in the WalletConnect network. Essentially it lets you use your project id and allow list to prevent malicious access.
+
+// The Blockchain API is the RPC service that powers AppKit's blockchain functions such as account balances, ENS resolution, transaction history, and more.
+// The Blockchain API is free for 6 million requests per 30 days.
+
+// https://docs.walletconnect.com/appkit/next/cloud/analytics => Analytics overview
+
+// https://docs.walletconnect.com/appkit/next/cloud/analytics => Notification API setup
+
 export default function Home() {
   return (
     <section className="px-4 py-2">
