cd2ca9f665ab0d1acebe7d90e875e1ef3ebcf131
Klemen Pavlovic <klemenpavlovic@gmail.com>
2024-09-12 14:09:03 +0200

    Add _headers file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {};
-// Next.js relies on SSR so we need to add this code here to make Web3Modal work properly
+
 const nextConfig = {
+  swcMinify: true,
+  // Next.js relies on SSR so we need to add this code here to make Web3Modal work properly
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
